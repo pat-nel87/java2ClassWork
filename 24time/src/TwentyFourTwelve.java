a48c4b695d1b84b46d8ecb99bc40c19f8dcdcc39
pat-nel87 <patnelson2022@gmail.com>
2021-07-26 20:20:49 -0400

    24 hour time format to 12 hour time format (Class work)

---
+++ 24time/src/TwentyFourTwelve.java
@@ -0,0 +1,36 @@
+import java.text.SimpleDateFormat;
+import java.text.DateFormat;
+import java.util.Date;
+import java.text.ParseException;
+
+public class TwentyFourTwelve {
+
+    public static String newTime;
+
+    public String convert(String originalTime) throws ParseException {
+        //System.out.println(originalTime);
+        DateFormat inputFormat = new SimpleDateFormat("HH:mm:ss");
+        DateFormat outputFormat = new SimpleDateFormat("hh:mm:ss aa");
+        Date givenDate = null;
+        givenDate = inputFormat.parse(originalTime);
+
+        return outputFormat.format(givenDate);
+    }
+
+    TwentyFourTwelve(String originalTime) {
+        try {
+            this.newTime = convert(originalTime);
+        } catch(ParseException x) {
+           System.out.println("Illegal Time Format Entry!!");
+        }
+
+    }
+
+    public static void main(String[] args) {
+        TwentyFourTwelve x = new TwentyFourTwelve("22:22:12");
+        System.out.println(x.newTime);
+        TwentyFourTwelve y = new TwentyFourTwelve("#4!567");
+
+    }
+
+}
